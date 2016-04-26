9f5306a333668858e744f145e26ee79a99c1c8f0
陈东东 <kevinchen@infapp.com>
2016-04-26 22:07:17 +0800

    no message

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'kevinchen:dlmenu',
-    version: '0.0.2',
+    version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: 'heytz dlmenu',
+    summary: '',
     // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/chendong0120/meteor-dlmenu.git',
+    git: '',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
@@ -13,12 +13,7 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
     api.addFiles('modernizr-custom.js', 'client');
+    api.use("fortawesome:fontawesome");
     api.addFiles('component.css', 'client');
-    api.addAssets([
-        'fonts/icomoon.dev.svg',
-        'fonts/icomoon.svg',
-        'fonts/icomoon.ttf',
-        'fonts/icomoon.woff',
-        'fonts/icomoon.eot'], ["client"]);
     api.addFiles('jquery.dlmenu.js', 'client');
 });
