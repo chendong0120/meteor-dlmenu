76ef8effb3255bec93ac3adb7123f6b8878e66d2
陈东东 <kevinchen@infapp.com>
2017-05-12 23:57:05 +0800

    no message

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
     name: 'kevinchen:dlmenu',
     version: '0.0.1',
-    // Brief, one-line summary of the package.
-    summary: '',
-    // URL to the Git repository containing the source code for this package.
-    git: '',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
+    summary: 'heytz dlmenu',
+    git: 'https://github.com/chendong0120/meteor-dlmenu.git',
     documentation: 'README.md'
 });
 
